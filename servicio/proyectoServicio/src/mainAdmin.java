1398a2bf1d6841354cb6456e1ac456203bcd6e3a
angelaguayo <angelaguayo@angelaguayo-pc>
2017-03-05 21:21:23 -0600

    cambiando administrador de control versiones

--- servicio/proyectoServicio/src/mainAdmin.java
+++ servicio/proyectoServicio/src/mainAdmin.java
@@ -2,6 +2,8 @@
 import java.awt.Color;
 
 /*
+
+calando esta mamada git en betbeans
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
